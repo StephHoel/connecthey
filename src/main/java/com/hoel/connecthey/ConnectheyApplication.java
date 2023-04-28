0632a0ab778d55331ea86278d75b4b7cc27fb80a
Steph Hoel <steph.hoel@gmail.com>
2023-04-28 18:39:44 -0300

    Initial Config Application

---
+++ src/main/java/com/hoel/connecthey/ConnectheyApplication.java
@@ -0,0 +1,13 @@
+package com.hoel.connecthey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectheyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectheyApplication.class, args);
+	}
+
+}
