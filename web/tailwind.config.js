b088f7875fd5a1bfa46643cb9209b99739d5ed3d
Steph Hoel <steph.hoel@gmail.com>
2023-04-30 21:47:51 -0300

    Update Config Web Project

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
