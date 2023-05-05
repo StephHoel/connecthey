a590e2bf8f71780c45ae0ad4886243186faf0d98
Steph Hoel <steph.hoel@gmail.com>
2023-05-05 10:41:52 -0300

    Update User Repository

--- src/main/java/com/hoel/connecthey/repository/UserRepository.java
+++ src/main/java/com/hoel/connecthey/repository/UserRepository.java
@@ -1,5 +1,25 @@
 package com.hoel.connecthey.repository;
 
-public class UserRepository {
-   
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.hoel.connecthey.model.UserModel;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+
+   @Query(value = "select * from USERS where NAME_USER = ?1 and PASSWORD_USER = ?2", nativeQuery = true)
+   UserModel findByLogin(String name, String pass);
+
+   // @Query(value = "select * from USER where LOWER(NAME_USER) LIKE '%' || ?1 ||
+   // '%'", nativeQuery = true)
+   // List<UserModel> findAllByName(String name);
+
+   // @Query(value = "select * from USER where LOWER(CNPJ_CPF_USER) LIKE '%' || ?1
+   // || '%'", nativeQuery = true)
+   // List<UserModel> findAllByDoc(String doc);
+
 }
